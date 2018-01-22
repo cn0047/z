c3be284491edec5e612c7cd7d7220a582e101570
Vladimir Kovpak <cn007b@gmail.com>
2018-01-22 18:33:57 +0200

    Init.

---
+++ src/config/main.template.js
@@ -0,0 +1,19 @@
+const config = {
+  apiUrl: 'https://api.*.com/v1',
+  aws: {
+    mail: {
+      accessKeyId: '',
+      secretAccessKey: '',
+      region: 'eu-west-1'
+    },
+    bucket: 'bucket'
+  },
+  auth: {
+    api_key: '',
+    app_type_id: '',
+    email: '',
+    password: ''
+  }
+};
+
+export default config;
